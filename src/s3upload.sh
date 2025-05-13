096760240632a2ec67d6002bd1f3b5877b4e8fe1
CohenRus <cohen_russell@icloud.com>
2025-05-13 13:01:20 -0700

    everyhing should be working, time to test it

--- src/s3upload.sh
+++ src/s3upload.sh
@@ -17,19 +17,25 @@ uploadOptionSelected()
         bucketname="$1"
     fi
 
-    # upload all files in selected to the bucket
-    
+    # get array of the paths to all the selected files, and upload each file
+    selectedfiles_array=("${(f)$(< "$USER_SELECTED_PATHH")}")
+    for file in "${selectedfiles_array[@]}"; do
+        aws s3 cp "$file" "$bucketname" --recursive --profile "$(cat "$USER_PROFILE_PATH")"
+    done
 }
 
 uploadOptionChanged()
 {
     bucketname=""
-
     # aquire the bucketname to upload to
     if [[ "$1" == "" ]]; then
         # not specified to look into log to find it
         if [[ -d "./.my-aws" ]]; then
-            bucketname="$(cat ./.my-aws/head)";
+            bucketname="$(cat ./.my-aws/head)"
+        elif [[ -d "../.my-aws" ]]; then
+            bucketname="$(cat ../.my-aws/head)"
+        elif [[ -d "../../.my-aws" ]]; then
+            bucketname="$(cat ../../.my-aws/head)"
         else
             echo "no myaws log found. Create one with myaws create-log, or"
             echo "specify which bucket to upload to in form s3://bucket-name/"
@@ -40,5 +46,5 @@ uploadOptionChanged()
     fi
 
     # upload all changed files in current directory to the bucket
-    aws s3 sync "$PWD" "$bucketname"
+    aws s3 sync "$PWD" "$bucketname" --profile "$(cat "$USER_PROFILE_PATH")"
 }
